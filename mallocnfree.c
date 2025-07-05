82949644114f472ce848c5484ebf2d0a8d3ced7e
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-07-05 18:48:09 +0300

    realloc added to malloc drill

--- mallocnfree.c
+++ mallocnfree.c
@@ -3,13 +3,27 @@
 
 
 int main(){
-  int num_of_vals = 50;
+  int num_of_vals = 10;
   int* ptr = malloc(num_of_vals * sizeof(int));
   // filling with values
   for (int i = 0; i < num_of_vals; i++){
       ptr[i] = i + 5;
-      printf("number %i is %i\n", i, ptr[i]);
+      printf("first ptr number %i is %i\n", i, ptr[i]);
   }
   printf("size of pointer is %lu\n", sizeof(ptr)); // returns the size of the pointer object
+
+  int new_vals = 2 * num_of_vals;
+  int* temp = realloc(ptr, new_vals * sizeof(int));
+  if (temp == NULL) {
+    perror("error reallocating memory");
+    exit(-1);
+  }
+  ptr = temp;
+
+  for (int i = 0; i < new_vals; i++){
+      ptr[i] = i * 2;
+      printf("second ptr number %i is %i\n", i, ptr[i]);
+  }
+  
   free(ptr);
 }
