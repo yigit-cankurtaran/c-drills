3e3968610433963afbbeaaa1c651ce8798d5d544
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-10-17 19:23:06 +0300

    improvements

--- fundamentals/my_strlen.c
+++ fundamentals/my_strlen.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-int my_strlen(char* string){
+
+// returning size_t because strings can be very long
+size_t my_strlen(char* string){
   // iterate through string to get size
   size_t str_size = 0;
-  while (string[str_size] != '\0') { // '\0'=EOF
+  while (string[str_size] != '\0') { // '\0'=null term
     str_size++;
   }
 
-  return str_size / sizeof(char);
+  // no need to divide by sizeof(char), that's always 1
+  return str_size;
 }
 
 int main(){
   char* hi = "hi";
   char* longtest = "hihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihi";
-  printf("size of first test %d\n", my_strlen(hi));
-  printf("size of long test %d\n", my_strlen(longtest));
+  printf("size of first test %zd\n", my_strlen(hi)); 
+  printf("size of long test %zd\n", my_strlen(longtest));
 }
