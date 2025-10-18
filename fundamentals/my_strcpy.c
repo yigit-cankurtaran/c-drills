e7c00a581d25661388ee3c23e383e8b40d7f2f06
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-10-18 20:45:57 +0300

    error on my strcpy, will fix

--- fundamentals/my_strcpy.c
+++ fundamentals/my_strcpy.c
@@ -1,11 +1,28 @@
-#include <string.h> // to test actual strcpy
+#include <string.h> // default strcpy and strlen
 #include <stdio.h> // for printf
 
-int main(){
-  char str1[20] = "hello world";
-  char str2[20] = "test";
+void my_strcpy(char* str1, char* str2){
+  if (!str1 || !str2){
+    printf("use valid strings");
+  }
 
-  printf("str2 before strcpy = %s\n", str2);
-  strcpy(str2, str1);
-  printf("str2 after strcpy = %s\n", str2);
+  // TODO: str2 prints a lot of "l"s, look into it and make this work properly
+  for (int i = 0; i < strlen(str1); i++){
+    str2[i] = str1[2];
+  }
+}
+
+int main(){
+  char strone[20] = "hello world";
+  char strtwo[20] = "test";
+
+  printf("strtwo before strcpy = %s\n", strtwo);
+  strcpy(strtwo, strone);
+  printf("strtwo after strcpy = %s\n", strtwo);
+
+  char str1[20] = "hello world1";
+  char str2[20] = "test2";
+  printf("str2 before my strcpy = %s\n", str2);
+  my_strcpy(str1, str2);
+  printf("str2 after my strcpy = %s\n", str2);
 }
